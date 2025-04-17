32c632907a060997b5108d094eb2b45d99fc1216
matshp0 <matvii.shpyruk@gmail.com>
2025-04-17 13:11:34 +0300

    add function to calculate postfix

--- implementation.go
+++ implementation.go
@@ -1,9 +1,77 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"math"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+var opMap = map[string]func(float64, float64) (float64, error){
+	"+": func(a, b float64) (float64, error) { return a + b, nil },
+	"-": func(a, b float64) (float64, error) { return a - b, nil },
+	"*": func(a, b float64) (float64, error) { return a * b, nil },
+	"/": func(a, b float64) (float64, error) {
+		if b == 0 {
+			return 0, errors.New("division by zero")
+		}
+		return a / b, nil
+	},
+	"^": func(a, b float64) (float64, error) { return math.Pow(a, b), nil },
+}
+
+type Operand struct {
+	number float64
+	symbol string
+}
+
+func ParseOperands(str string) ([]Operand, error) {
+	var result []Operand
+	arr := strings.Split(str, " ")
+	for _, el := range arr {
+		var parsed Operand
+		value, err := strconv.ParseFloat(el, 64)
+		if err != nil {
+			_, ok := opMap[el]
+			if ok != true {
+				return nil, errors.New("invalid operator")
+			}
+			parsed.symbol = el
+		} else {
+			parsed.number = value
+		}
+		result = append(result, parsed)
+	}
+	return result, nil
+}
+
+func CalculatePostfix(input string) (float64, error) {
+	operands, err := ParseOperands(input)
+	if err != nil {
+		return 0, err
+	}
+	i := 0
+	for len(operands) != 1 {
+		if operands[i].symbol == "" {
+			i++
+			if i == len(operands) {
+				return 0, errors.New("invalid expression")
+			}
+			continue
+		}
+		if i < 2 {
+			return 0, errors.New("invalid expression")
+		}
+		v1, v2 := operands[i-2].number, operands[i-1].number
+		operation := opMap[operands[i].symbol]
+		value, err := operation(v1, v2)
+		if err != nil {
+			return 0, err
+		}
+		var result Operand
+		result.number = value
+		operands = append(operands[:i-2], append([]Operand{result}, operands[i+1:]...)...)
+		i -= 2
+	}
+	return operands[0].number, nil
 }
--- implementation_test.go
+++ implementation_test.go
@@ -2,20 +2,19 @@ package lab2
 
 import (
 	"fmt"
-	"testing"
-
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestCalculatePostfix(t *testing.T) {
+	res, err := CalculatePostfix("4 2 23 3 3 3 3 3")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, 23.0, res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExampleCalculatePostfix() {
+	res, _ := CalculatePostfix("2 2 +")
 	fmt.Println(res)
 
 	// Output:
